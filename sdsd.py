495aab7a2f91cb162c92d45b8b8dcce84e14e34f
DEB <112969476+Mot932@users.noreply.github.com>
2023-04-21 19:42:53 +0300

    Add files via upload

--- sdsd.py
+++ sdsd.py
@@ -1,6 +1,7 @@
 import pygame
 import sys
-import degrees_to_velocity
+from degrees_to_velocity import degrees_to_velocity
+import random
 
 pygame.init()
 WHITE = (255, 255, 255)
@@ -21,23 +22,46 @@ pygame.display.set_caption("Моя программа")
 player_1_width = 20
 player_1_height = 150
 player_x_1 = 50
+player_1_score = 0
 player_y_1 = screen_height // 2 - player_1_height // 2
 player_1 = pygame.Rect((player_x_1, player_y_1, player_1_width, player_1_height))
+player_1_speed = 8
 player_2_width = 20
 player_2_height = 150
+player_2_score = 0
 player_x_2 = screen_width - player_1_width - 50
 player_y_2 = screen_height // 2 - player_2_height // 2
 player_2 = pygame.Rect((player_x_2, player_y_2, player_2_width, player_2_height))
-
+player_2_speed = 8
 
 ball_width = 15
 ball_height = 15
 ball_x = screen_width // 2 - ball_width // 2
 ball_y = screen_height // 2 - ball_height // 2
-velocity = degrees_to_velocity.degrees_to_velocity(60, 10)
+velocity = degrees_to_velocity(60, 7)
 ball_speed_x = velocity[0]
 ball_speed_y = velocity[1]
 ball = pygame.Rect((ball_x, ball_y, ball_height, ball_width))
+
+def ball_to_center():
+    ball.x = ball_x
+    
+"""
+    if random.randint(0, 1) == 0:
+        velocity = degrees_to_velocity(random.randint(215, 305), 7)
+    else:
+        velocity = degrees_to_velocity(random.randint(215, 305), 7)
+    
+    ball_speed_x = velocity[0]
+    ball_speed_y = velocity[1]
+"""
+    
+    
+    
+score_right = pygame.font.Font(None, 60)
+score_left = pygame.font.Font(None, 60)
+
+
 
 game = True
 while game: # главный цикл
@@ -53,32 +77,37 @@ while game: # главный цикл
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_UP]:
-        player_2.y -= 8
+        player_2.y -= player_1_speed
         if player_2.y < 0:
             player_2.y = 0
     if keys[pygame.K_DOWN]:
-        player_2.y += 8
+        player_2.y += player_1_speed
         if player_2.y > screen_height - player_2_height:
             player_2.y = screen_height - player_2_height
     if keys[pygame.K_w]:
-        player_1.y -= 8
+        player_1.y -= player_2_speed
         if player_1.y < 0:
             player_1.y = 0
     if keys[pygame.K_s]:
-        player_1.y += 8
+        player_1.y += player_2_speed
         if player_1.y > screen_height - player_1_height:
             player_1.y = screen_height - player_1_height
 
     ball.x += ball_speed_x
     ball.y += ball_speed_y
     if ball.x < 0:
-        ball_speed_x *= -1
+        player_2_score += 1
+        ball_to_center()
     if ball.x > screen_width - ball_width:
-        ball_speed_x *= -1
+        player_1_score += 1
+        ball_to_center()
     if ball.y < 0:
         ball_speed_y *= -1
     if ball.y > screen_height - ball_height:
         ball_speed_y *= -1
+
+    if ball.colliderect(player_1) or ball.colliderect(player_2):
+        ball_speed_x *= -1
 
 
     # отрисовка
@@ -86,6 +115,17 @@ while game: # главный цикл
     pygame.draw.rect(screen, WHITE, player_1)
     pygame.draw.rect(screen, WHITE, player_2)
     pygame.draw.rect(screen, WHITE, ball)
+    score_left_img = score_left.render(str(player_1_score), True, WHITE)
+    score_right_img = score_right.render(str(player_2_score), True, WHITE)
+    screen.blit(score_left_img, (screen_width * 0.25, 20))
+    screen.blit(score_right_img, (screen_width * 0.75, 20))
+    line = pygame.draw.line(
+        screen,
+        WHITE, 
+        [screen_width // 2, 0], 
+        [screen_width // 2, screen_height],
+        5)
+    
     pygame.display.flip()
 
     clock.tick(100)
